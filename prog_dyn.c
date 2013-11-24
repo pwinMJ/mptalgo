69b76fdebea30e63b173950760a499f0e9529264
tonnellier <jerome1tonnelier@msn.com>
2013-11-24 03:46:12 +0100

    ajout du rapport.txt et modif prog_dyn

--- prog_dyn.c
+++ prog_dyn.c
@@ -14,21 +14,12 @@ void affiche_mots(int nb_mots, char ** liste_mots ){
 
 int main(int argc, char ** argv){
 
-  struct liste_sol * ls = NULL;
-  //int taille_ls;
-
-  ls = alloue_element(ls);
-  ls->i =1;
-  ls->j= 2;
-  ls->suivant = (struct liste_sol *) 3;
-  ls->suivant = alloue_element(ls->suivant);
-  ls->suivant->i = 3;
-  ls->suivant->j = 4;
+  struct liste_sol * ls;
+    alloue_element(ls , 1, 2);
     
-  ajoute_element(ls, 7, 8);
-  ajoute_element(ls,  7, 8);
-ajoute_element(ls,  7, 8);
-ajoute_element(ls,  7, 8);
+  ajoute_element(ls,  3, 5);
+ajoute_element(ls,  6, 8);
+ajoute_element(ls,  9, 10);
 
  affiche(ls);
   
