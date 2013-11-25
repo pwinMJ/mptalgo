b1aa5f46a6d96774cb60191f14cf76e517679fba
tonnellier <jerome1tonnelier@msn.com>
2013-11-25 16:04:48 +0100

    avancement dans affiche_mots

--- prog_dyn.c
+++ prog_dyn.c
@@ -3,6 +3,7 @@
 #include "liste_solution.c"
 #include "ens_mots.c"
 #define ESPACE ' '
+#define CHAR_FIN_LIGNE '*'
 
 /*
 struct liste_sol * prog_dyn(char ** liste_mots, int max){
@@ -13,40 +14,47 @@ struct liste_sol * prog_dyn(char ** liste_mots, int max){
 }
 */
 
-//TODO: A METTRE DANS main.c
+//TODO
 void affiche_mots(char ** liste_mots, struct liste_sol * ls, int max){
-  int i, taille_mot, taille_ligne ,ecrits;
+  int i, taille_mot, reste_ligne ,ecrits;
   char buf[max+3];
   char * buf_cour;
-  char espace = ' ';
+
   buf[0] = '|';
   buf[max+1] = '|';
   buf[max+2] = '\n';
  
   
   while(ls != NULL){
-    taille_ligne = max;
+    
+    reste_ligne = max;
     buf_cour = buf+1;
+    
     for(i = ls->i; i <= ls->j; i++){
-      buf_cour = strcpy(buf_cour, liste_mots[i]);
-      taille_mot = strlen(liste_mots[i]);
-      taille_ligne -= taille_mot;
-      buf_cour += taille_mot;
-      if(0 < taille_ligne){
-	*buf_cour = espace;
+      if(ls->i < i){
+	*buf_cour = ESPACE;
 	buf_cour++;
+	reste_ligne--;
       }
+      taille_mot = strlen(liste_mots[i]);
+      if(reste_ligne < taille_mot){
+	printf("erreur:affiche_mots():taille mot trop grande\n");
+	return;
+      }
+      buf_cour = strncpy(buf_cour, liste_mots[i],taille_mot); 
+      reste_ligne -= taille_mot;
+      buf_cour += taille_mot;
     }
-    for(i=(max-taille_ligne); i <= max; i++){
-      buf[i] = espace;
+    for(i=(max-reste_ligne)+1; i <= max; i++){
+      buf[i] = CHAR_FIN_LIGNE;
     }
-    ecrits = write(STDOUT_FILENO, buf, max+2);
+    
+    ecrits = write(STDOUT_FILENO, buf, max+3);
     if(ecrits < 0){
       perror("write:affiche_mots()");
       return;
     }
     
-    printf("\n");
     ls = ls->suivant;
 
   }
@@ -63,11 +71,14 @@ int main(int argc, char ** argv){
   char ** liste_mots = cons_liste_mots(argv[1]);
 
   struct liste_sol * ls = NULL;
-  ls = ajoute_element(ls , 0, 2);
-  ls = ajoute_element(ls,  3, 5);
-  ls = ajoute_element(ls,  6, 7);
-  affiche_mots(liste_mots, ls, 15);
+  ls = ajoute_element(ls , 0, 1);
+ 
+  ls = ajoute_element(ls,  2, 3);
+  ls = ajoute_element(ls,  4, 4);
+
+  affiche_mots(liste_mots, ls, 10);
   
+  free(liste_mots);
   free(ls);
   return 0;
 }
