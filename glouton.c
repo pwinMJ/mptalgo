8fb71e739b3fe397441b4828c2fb91b07c8014c7
pwinMJ <meryam1905@gmail.com>
2013-11-27 18:42:29 +0100

    modif glouton.c

--- glouton.c
+++ glouton.c
@@ -1,58 +1,54 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include "affichage.c"
 
 
-
-
-int glouton (char** mots, int nbmots,int m){
-  //m: taille de la ligne
-  //nbmots : nbmots dans la list
-  
-  int deb = 0 ,i = 0;
-  int taille = m;	
-  printf("i=%d , deb =%d",i,deb); 
-      while(i < nbmots ){
-    taille = m;
-    //deb : l'indice du premier mot de la liste achaque iteration
-    deb=i;
+struct liste_sol * glouton ( char** mots, int nbmots,int m){
+  int i = 1,deb,fin;
+  int taille;
+  struct liste_sol * ls;
+    printf("i1=%d \n",i);
+      while(i < nbmots ){ 
+    deb =i;  
+    taille = m;  
     //cas du premier mot de la ligne
-    if(strlen(mots[i]) <= m){
-      taille=taille - strlen(mots[i]);
+    printf("i2=%d",i);
+    if(strlen(mots[i]) <= m && taille>0){
+      taille=taille - strlen(mots[i]);   
+      //printf("%s",mots[i]);      
       i++;
     }
     
-    else
-      //mots trop long
-      printf("ce mots %s est plus long que la ligne ",mots[i]); 
-    printf("i=%d , deb =%d",i,deb); 
-    //cas des mots suivants
-      printf("%s",mots[i]);
-	
-	while(strlen(mots[i]) <= taille  ){
-	printf("%s",mots[i]);
-	if (strlen(mots[i])<taille )
-	  taille=taille - ( strlen(mots[i])) - 1;      
-	else taille=taille - (strlen(mots[i]));
-	i++;
-      }
-	
-    //printf("%s",mots[deb]);
-    deb++;
-    printf("i=%d , deb =%d",i,deb); 
-    /* 
-    while(deb<i){
-     printf(" %s",mots[deb]);
-     
-     }    
-    */
+    if(i==nbmots) {
+      // printf("\n");
+      break;
+    }
+    //cas des autres mots
+    printf("i3=%d",i);
+    while( (strlen(mots[i]) +1) <= taille){
+      //printf(" %s",mots[i]);
+      //if (strlen(mots[i])<taille )
+      taille=taille - ( strlen(mots[i])) - 1;      
+      i++;     
+      printf("fin = %d",fin);
+      //condition darret
+      if(i==nbmots) break;
+    }//fin while
+      fin=i-1;
+      ls=ajoute_element(ls,deb,fin);
   }
-  return 0;
-  
-  
-} 
+    
+  return ls;
+}
+
+
+
 int main(int argc, char ** argv){
-  printf(" %d",argc); 
-  glouton(argv, argc, 17);
+  struct liste_sol*ls=NULL; 
+  printf("argc =%d\n",argc);
+  ls= glouton(argv, argc, 17);
+  //affiche_sol(ls);
+  //affiche_mots(argv,ls,17);
   return 0;
 }
