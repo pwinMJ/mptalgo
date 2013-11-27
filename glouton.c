032d25448f376294f3ac16afbeb9c1c77b2874b4
pwinMJ <meryam1905@gmail.com>
2013-11-27 23:07:59 +0100

    glouton avec argv =>fichier pas encore

--- glouton.c
+++ glouton.c
@@ -2,53 +2,44 @@
 #include<stdlib.h>
 #include<string.h>
 #include "affichage.c"
-
-
+#include "ens_mots.c"
 struct liste_sol * glouton ( char** mots, int nbmots,int m){
-  int i = 1,deb,fin;
+  int i = 1,deb=0,fin=0;
+  struct liste_sol * ls =NULL;
   int taille;
-  struct liste_sol * ls;
-    printf("i1=%d \n",i);
-      while(i < nbmots ){ 
-    deb =i;  
+  while(i < nbmots ){
+ 
     taille = m;  
+    deb=i;
     //cas du premier mot de la ligne
-    printf("i2=%d",i);
     if(strlen(mots[i]) <= m && taille>0){
-      taille=taille - strlen(mots[i]);   
-      //printf("%s",mots[i]);      
+      taille=taille - strlen(mots[i]);        
       i++;
-    }
+    }    
+    if(i==nbmots)  break;
     
-    if(i==nbmots) {
-      // printf("\n");
-      break;
-    }
     //cas des autres mots
-    printf("i3=%d",i);
-    while( (strlen(mots[i]) +1) <= taille){
-      //printf(" %s",mots[i]);
-      //if (strlen(mots[i])<taille )
+    while(strlen(mots[i]) < taille && taille>0){
       taille=taille - ( strlen(mots[i])) - 1;      
-      i++;     
-      printf("fin = %d",fin);
+      i++;
+      
       //condition darret
       if(i==nbmots) break;
     }//fin while
-      fin=i-1;
-      ls=ajoute_element(ls,deb,fin);
+    fin=i-1;
+    ls=ajoute_element(ls,deb,fin);
   }
-    
   return ls;
 }
 
 
 
 int main(int argc, char ** argv){
-  struct liste_sol*ls=NULL; 
-  printf("argc =%d\n",argc);
+  struct liste_sol * ls =NULL;
+  printf("\n argc = %d \n",argc);
+  //NE MARCHE PAS 
+  //char ** fich=cons_liste_mots(argv[1]);
   ls= glouton(argv, argc, 17);
-  //affiche_sol(ls);
-  //affiche_mots(argv,ls,17);
+  affiche_mots(argv,ls,17);
   return 0;
 }
